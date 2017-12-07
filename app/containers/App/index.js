f87f6b403cca8f3ca0a69852536828dcb450f7fc
bhefty <billhefty@gmail.com>
2017-12-06 20:30:28 -0600

    add basic logout/login button for testing

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -2,20 +2,38 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { loginRequest, logout } from 'auth/actions'
+import { makeSelectIsAuthenticated } from 'auth/selectors'
 
 export class App extends React.PureComponent {
   static propTypes = {
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired
   }
 
   render () {
     return (
       <div>
         <h3>Nav here</h3>
+        {this.props.isAuthenticated
+        ? <button onClick={this.props.logout}>Logout</button>
+        : <button onClick={this.props.login}>Login</button>
+        }
         {React.Children.toArray(this.props.children)}
       </div>
     )
   }
 }
 
-export default withRouter(connect(null, null)(App))
+const mapStateToProps = createStructuredSelector({
+  isAuthenticated: makeSelectIsAuthenticated()
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  login: () => dispatch(loginRequest()),
+  logout: () => dispatch(logout())
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
