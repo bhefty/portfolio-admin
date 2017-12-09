b88dd7175ad7a7de9cafe5e5ae6a1506350ea48a
bhefty <billhefty@gmail.com>
2017-12-09 09:13:57 -0600

    add login logic to HomePage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { createStructuredSelector } from 'reselect'
+
+import { loginRequest } from 'auth/actions'
+import { makeSelectIsAuthenticated } from 'auth/selectors'
+
+import Wrapper from './Wrapper'
 
 export class HomePage extends React.Component {
   render () {
     return (
-      <h1>Oi! You cannot be here without a password!</h1>
+      <Wrapper>
+        <h1>Home</h1>
+        {!this.props.isAuthenticated
+          ? <button onClick={this.props.login}>Login</button>
+          : <Link to='/dashboard'>Dashboard</Link>
+        }
+      </Wrapper>
     )
   }
 }
 
-export default HomePage
+const mapStateToProps = createStructuredSelector({
+  isAuthenticated: makeSelectIsAuthenticated()
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  login: () => dispatch(loginRequest())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
