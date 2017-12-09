6ffb47577718cf4e12749ae63353a6f0447a62b2
bhefty <billhefty@gmail.com>
2017-12-09 09:14:41 -0600

    expose AUTH0_CALLBACK_URL to clientside

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -74,7 +74,8 @@ module.exports = (options) => ({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         AUTH0_CLIENT_ID: JSON.stringify(process.env.AUTH0_CLIENT_ID),
-        AUTH0_DOMAIN: JSON.stringify(process.env.AUTH0_DOMAIN)
+        AUTH0_DOMAIN: JSON.stringify(process.env.AUTH0_DOMAIN),
+        AUTH0_CALLBACK_URL: JSON.stringify(process.env.AUTH0_CALLBACK_URL)
       }
     }),
     new webpack.NamedModulesPlugin()
