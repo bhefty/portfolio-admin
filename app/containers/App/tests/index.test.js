3a320e49c43515427d9bbe7f341acd6cc669adb3
bhefty <billhefty@gmail.com>
2017-12-09 18:37:11 -0600

    pass in isAuthenticated and location props for testing

--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -6,8 +6,12 @@ import { App } from '../index'
 describe('<App />', () => {
   it('should render its children', () => {
     const children = (<h1>Test</h1>)
+    const isAuthenticated = false
+    const location = {
+      pathname: '/'
+    }
     const renderedComponent = shallow(
-      <App>
+      <App isAuthenticated={isAuthenticated} location={location}>
         {children}
       </App>
     )
