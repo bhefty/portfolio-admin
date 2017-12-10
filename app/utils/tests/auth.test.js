f4d7b238e0dba3a9ca9437ac3f4885818e24c385
bhefty <billhefty@gmail.com>
2017-12-09 18:37:38 -0600

    add testing support for accessToken and expiresIn states

--- app/utils/tests/auth.test.js
+++ app/utils/tests/auth.test.js
@@ -1,7 +1,10 @@
 import localStorage from 'localStorage'
+import MockDate from 'mockdate'
 
 import {
+  ACCESS_TOKEN,
   ID_TOKEN,
+  EXPIRES_AT,
   PROFILE,
   setStoredAuthState,
   removeStoredAuthState,
@@ -9,12 +12,16 @@ import {
 } from '../auth'
 
 const mockedStorage = {
-  idToken: 'abc123BCA321',
+  accessToken: '123abc',
+  idToken: 'abc123ABC321',
+  expiresIn: 7200,
   profile: {
     email: 'joeschmoe@example.com',
     name: 'Joe Schmoe'
   }
 }
+MockDate.set(new Date().getTime())
+const expiresAt = JSON.stringify((mockedStorage.expiresIn * 1000) + new Date().getTime())
 
 describe('auth', () => {
   beforeEach(() => {
@@ -23,19 +30,23 @@ describe('auth', () => {
 
   describe('setStoredAuthState', () => {
     it('should set localStorage with profile and idToken', () => {
-      setStoredAuthState(mockedStorage.profile, mockedStorage.idToken)
+      setStoredAuthState(mockedStorage.profile, mockedStorage.accessToken, mockedStorage.idToken, mockedStorage.expiresIn)
 
-      expect(localStorage.length).toEqual(2)
+      expect(localStorage.length).toEqual(4)
+      expect(localStorage.getItem(ACCESS_TOKEN)).toEqual(mockedStorage.accessToken)
       expect(localStorage.getItem(ID_TOKEN)).toEqual(mockedStorage.idToken)
+      expect(localStorage.getItem(EXPIRES_AT)).toEqual(expiresAt)
       expect(JSON.parse(localStorage.getItem(PROFILE))).toEqual(mockedStorage.profile)
     })
   })
 
   describe('removeStoredAuthState', () => {
     it('should remove auth state from localStorage', () => {
+      localStorage.setItem(ACCESS_TOKEN, mockedStorage.accessToken)
       localStorage.setItem(ID_TOKEN, mockedStorage.idToken)
+      localStorage.setItem(EXPIRES_AT, expiresAt)
       localStorage.setItem(PROFILE, JSON.stringify(mockedStorage.profile))
-      expect(localStorage.length).toEqual(2)
+      expect(localStorage.length).toEqual(4)
 
       removeStoredAuthState()
 
@@ -44,15 +55,22 @@ describe('auth', () => {
   })
 
   describe('getStoredAuthState', () => {
-    it('should get localStorage values for idToken and profile', () => {
+    it('should get localStorage values for isAuthenticated and profile', () => {
+      localStorage.setItem(ACCESS_TOKEN, mockedStorage.accessToken)
       localStorage.setItem(ID_TOKEN, mockedStorage.idToken)
+      localStorage.setItem(EXPIRES_AT, expiresAt)
       localStorage.setItem(PROFILE, JSON.stringify(mockedStorage.profile))
-      expect(localStorage.length).toEqual(2)
+      expect(localStorage.length).toEqual(4)
 
-      const { idToken, profile } = getStoredAuthState()
+      const { isAuthenticated, profile } = getStoredAuthState()
 
-      expect(idToken).toEqual(mockedStorage.idToken)
+      expect(isAuthenticated).toBe(true)
       expect(profile).toEqual(mockedStorage.profile)
+    })
+
+    it('should return isAuthenticated as false if user has not already authenticated', () => {
+      const { isAuthenticated } = getStoredAuthState()
+      expect(isAuthenticated).toBe(false)
     })
   })
 })
