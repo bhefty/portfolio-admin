3ba0fe97782c61ae6eb13aa09a61c7d4639f1c5f
bhefty <billhefty@gmail.com>
2017-12-13 17:33:16 -0600

    create TopNav comp

---
+++ app/components/TopNav/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import TopNavWrapper from './TopNavWrapper'
+
+const TopNav = ({ id }) => {
+  return (
+    <TopNavWrapper id={id}>
+      <Link to='/'>Portfolio Admin</Link>
+      <button onClick={() => console.log('TODO: Logout function')}>Logout</button>
+    </TopNavWrapper>
+  )
+}
+
+TopNav.propTypes = {
+
+}
+
+export default TopNav
