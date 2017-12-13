08efb05c965289c100c8136d7ba49fcc536d3b77
bhefty <billhefty@gmail.com>
2017-12-13 17:31:45 -0600

    create SideNav comp

---
+++ app/components/SideNav/SideNavWrapper.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.nav`
+  background-color: #3D3F47;
+  a {
+    font-size: 1.5em;
+    text-decoration: none;
+    color: #fff;
+
+    &:active, &:hover {
+      color: #ccc;
+    }
+  }
+`
---
+++ app/components/SideNav/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import SideNavWrapper from './SideNavWrapper'
+
+const SideNav = ({ id }) => {
+  return (
+    <SideNavWrapper id={id}>
+      <Link to='/dashboard'>Dashboard</Link>
+      <Link to='/blog'>Blog Posts</Link>
+      <Link to='/projects'>Projects</Link>
+    </SideNavWrapper>
+  )
+}
+
+SideNav.propTypes = {
+
+}
+
+export default SideNav
