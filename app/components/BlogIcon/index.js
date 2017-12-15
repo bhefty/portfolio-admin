f0144fd7c58444ce07bb48fdf56155060af8c2e6
bhefty <billhefty@gmail.com>
2017-12-15 10:24:36 -0600

    change to enableBackground

--- app/components/BlogIcon/index.js
+++ app/components/BlogIcon/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BlogIcon = () => {
   return (
     <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' x='0px' y='0px'
-      viewBox='0 0 64 64' enable-background='new 0 0 64 64'>
+      viewBox='0 0 64 64' enableBackground='new 0 0 64 64'>
       <g>
         <g>
           <path d='M15,44.5c2.2,0.6,4.5,1,7,1c12.2,0,22-8.1,22-18s-9.8-18-22-18s-22,8.1-22,18c0,5.6,3.1,10.7,8,14c0,0,0,2.6-2.1,5.2
