b24421232fda567c8c7863becbb12c81ec4e5a84
bhefty <billhefty@gmail.com>
2017-12-16 14:00:06 -0600

    style 404 page

---
+++ app/containers/NotFoundPage/Wrapper.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  background-color: #3D3F47;
+  color: #fff;
+  height: 100vh;
+  width: 100%;
+  display: grid;
+  letter-spacing: 2px;
+  font-size: 2em;
+  
+  .container {
+    margin: auto;
+  }
+
+  .four04-comment {
+    color: #4F8C4B;
+  }
+
+  .four04-if {
+    color: #d65562;
+  }
+
+  .four04-not {
+    color: #2796ec;
+  }
+
+  .four04-exception {
+    color: #C684C1;
+  }
+
+  .four04-flip {
+    color: #CF916D;
+  }
+`
