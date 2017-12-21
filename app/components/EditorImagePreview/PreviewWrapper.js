f8eed493541fc267bf3256d04ea88ce38aab127d
bhefty <billhefty@gmail.com>
2017-12-20 21:58:46 -0600

    add preview component to show uploaded image

---
+++ app/components/EditorImagePreview/PreviewWrapper.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  height: 100px;
+  width: 100%;
+  margin: 0.5em 0 1em 0;
+
+  span {
+    position: relative;
+    top: -40px;
+    cursor: pointer;
+    
+    svg {
+      fill: #EB5757;
+    }
+  }
+  
+  img {
+    height: 100%;
+  }
+`
